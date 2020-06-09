bfe9d51dfe901ac89233ba701360dee21da57564
Melvin Davis <melvinodsa@gmail.com>
2020-06-09 20:08:37 +0530

    Added file uploader service

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+rm -rf repos
 mkdir repos
 git clone https://github.com/cuttle-ai/auth-service repos/auth-service
 git clone https://github.com/cuttle-ai/brain repos/brain
@@ -5,3 +6,5 @@ git clone https://github.com/cuttle-ai/octopus repos/octopus
 git clone https://github.com/cuttle-ai/configs repos/configs 
 git clone https://github.com/cuttle-ai/db-toolkit repos/db-toolkit
 git clone https://github.com/cuttle-ai/brain-frontend repos/brain-frontend
+docker-compose build
+docker-compose start
