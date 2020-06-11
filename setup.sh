b607df23124a18a8c70aa22b45a8d0a4ffedea6c
Melvin Davis <melvinodsa@gmail.com>
2020-06-11 21:08:21 +0530

    build fix

--- setup.sh
+++ setup.sh
@@ -6,5 +6,11 @@ git clone https://github.com/cuttle-ai/octopus repos/octopus
 git clone https://github.com/cuttle-ai/configs repos/configs 
 git clone https://github.com/cuttle-ai/db-toolkit repos/db-toolkit
 git clone https://github.com/cuttle-ai/brain-frontend repos/brain-frontend
+git clone https://github.com/cuttle-ai/octopus-service repos/octopus-service
+git clone https://github.com/cuttle-ai/octopus repos/octopus
+git clone https://github.com/cuttle-ai/brain repos/brain
+git clone https://github.com/cuttle-ai/data-integration-service repos/data-integration-service
+git clone https://github.com/cuttle-ai/file-uploader-service repos/file-uploader-service
+git clone https://github.com/cuttle-ai/go-sdk repos/go-sdk
 docker-compose build
 docker-compose start
