2d4f581d7abad95505cdb34d284439dfa3224b30
Melvin Davis <melvinodsa@gmail.com>
2020-06-21 22:16:27 +0530

    splitted setup and repos cloning

---
+++ repos.sh
@@ -0,0 +1,15 @@
+rm -rf repos
+mkdir repos
+git clone https://github.com/cuttle-ai/auth-service repos/auth-service
+git clone https://github.com/cuttle-ai/brain repos/brain
+git clone https://github.com/cuttle-ai/octopus repos/octopus
+git clone https://github.com/cuttle-ai/configs repos/configs 
+git clone https://github.com/cuttle-ai/db-toolkit repos/db-toolkit
+git clone https://github.com/cuttle-ai/brain-frontend repos/brain-frontend
+git clone https://github.com/cuttle-ai/octopus-service repos/octopus-service
+git clone https://github.com/cuttle-ai/octopus repos/octopus
+git clone https://github.com/cuttle-ai/brain repos/brain
+git clone https://github.com/cuttle-ai/data-integration-service repos/data-integration-service
+git clone https://github.com/cuttle-ai/file-uploader-service repos/file-uploader-service
+git clone https://github.com/cuttle-ai/go-sdk repos/go-sdk
+git clone https://github.com/cuttle-ai/websockets repos/websockets
--- setup.sh
+++ setup.sh
@@ -1,17 +1,4 @@
 docker-compose down
-rm -rf repos
-mkdir repos
-git clone https://github.com/cuttle-ai/auth-service repos/auth-service
-git clone https://github.com/cuttle-ai/brain repos/brain
-git clone https://github.com/cuttle-ai/octopus repos/octopus
-git clone https://github.com/cuttle-ai/configs repos/configs 
-git clone https://github.com/cuttle-ai/db-toolkit repos/db-toolkit
-git clone https://github.com/cuttle-ai/brain-frontend repos/brain-frontend
-git clone https://github.com/cuttle-ai/octopus-service repos/octopus-service
-git clone https://github.com/cuttle-ai/octopus repos/octopus
-git clone https://github.com/cuttle-ai/brain repos/brain
-git clone https://github.com/cuttle-ai/data-integration-service repos/data-integration-service
-git clone https://github.com/cuttle-ai/file-uploader-service repos/file-uploader-service
-git clone https://github.com/cuttle-ai/go-sdk repos/go-sdk
+sh repos.sh
 FILE_UPLOADER_PUBLIC_KEY=$(cat id_rsa.pub) FILE_UPLOADER_PRIVATE_KEY=$(cat id_rsa) docker-compose build
 docker-compose up
